b2c068114e6c3dd1320a7bac214ac02a508687fd
radadiyasmit5 <77418004+radadiyasmit5@users.noreply.github.com>
2022-01-10 04:04:33 -0500

    First Commit

---
+++ server/models/sub.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const slugify = require("slugify")
+const {ObjectId} = mongoose.Schema
+
+const subschema = new mongoose.Schema({
+
+
+    name: {
+        type: "String",
+        trim: true,
+        required: true,
+        minlength: [2, "name is to short"],
+        maxlength: [32, "name is to long"]
+    },
+    slug: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        index: true
+    },
+    parent:{type:ObjectId,ref:"Catagory",required:true},
+
+},
+    { timestamps: true })
+
+exports.Sub = mongoose.model("Sub", subschema)
