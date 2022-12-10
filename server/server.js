4c26dd4ed78d9e407279d0d64232d2668664c426
radadiyasmit2012 <radadiyasmit5@gmail.com>
2022-12-10 16:23:09 -0400

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const cors = require("cors")
-const morgan = require('morgan')
-const mongoose = require("mongoose")
-const bodyParser = require('body-parser')
-require('dotenv').config()
-const fs = require("fs")
-const { log } = require('console')
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const fs = require("fs");
+const { log } = require("console");
 const app = express();
 
-app.use(cors())
-app.use(morgan("dev"))
-app.use(bodyParser.json({ limit: "2mb" }))
-app.use(express.urlencoded({extended:true}))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // const authrout = require("./routes/auth")  --- there are lots of routes in real application so import all routes automatically we can use fs.readdirsync methoud and map it threw all the file and require it
 // app.use("/api",authrout) -- before i made it automate i was importin it like this manually
 
-fs.readdirSync("./routes").map((r) => { app.use("/api", require("./routes/" + r)) }) // ---  after automation it is importing routes and i also write app.use() function there and push path into it.
+app.get("/",(req,res)=>{
+  res.send("hello")
+})
 
+// fs.readdirSync("./routes").map((r) => {
+//   app.use("/api", require("./routes/" + r));
+// }); // ---  after automation it is importing routes and i also write app.use() function there and push path into it.
 
-
-
-
-
-mongoose.connect(process.env.DATABASE)
-
-
-
+// mongoose.connect(process.env.DATABASE);
 
 //now i am using routes middleware so i dont have to use this
 // app.get("/api", (req, res) => {
@@ -37,9 +35,7 @@ mongoose.connect(process.env.DATABASE)
 //     })
 // })
 
-
-
-let port=process.env.PORT||8000
+let port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log("port is running",port);
-})
+  console.log("port is running", port);
+});
