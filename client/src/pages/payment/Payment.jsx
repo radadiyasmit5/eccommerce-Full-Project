9933e0b347f7e2b316a56fe2ebc39dcb69125484
Smit Radadiya <smit.radadiya@nielsen.com>
2024-02-10 18:02:11 -0400

    added error page

--- client/src/pages/payment/Payment.jsx
+++ client/src/pages/payment/Payment.jsx
@@ -5,21 +5,39 @@ import {paymentIntent} from "../../functions/payment"
 import {useSelector} from "react-redux"
 import CheckoutForm from "../../componant/payment/CheckoutForm"
 import "./payment.css"
+import {toast} from "react-toastify"
+import {useHistory} from "react-router-dom"
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_TOKEN)
 export default function Payment() {
   const {user, coupon} = useSelector((state) => state)
+  const history = useHistory()
   const [clientSecret, setclientSecrete] = useState("")
   const [originalTotal, setoriginalTotal] = useState(0)
   const [finalTotal, setfinalTotal] = useState(0)
   const [totalPriceAfterDiscount, settotalPriceAfterDiscount] = useState(0)
   useEffect(() => {
     if (user && user.token) {
-      paymentIntent(user.token, coupon).then((res) => {
-        setclientSecrete(res.data.clientSecret)
-        setoriginalTotal(res.data.originalTotal)
-        setfinalTotal(res.data.finalTotal)
-        settotalPriceAfterDiscount(res.data.totalPriceAfterDiscount)
-      })
+      paymentIntent(user.token, coupon)
+        .then((res) => {
+          setclientSecrete(res.data.clientSecret)
+          setoriginalTotal(res.data.originalTotal)
+          setfinalTotal(res.data.finalTotal)
+          settotalPriceAfterDiscount(res.data.totalPriceAfterDiscount)
+        })
+        .catch((err) => {
+          console.log(err)
+          toast.error(
+            "Sorry ,Server Encountered SOme issue While generating Payment Link"
+          )
+          history.push({
+            pathname: "/error",
+            state: {
+              from: "/payment",
+              err: "Failed to get the payment Intent",
+              to: "/checkout",
+            },
+          })
+        })
     }
   }, [user])
 
